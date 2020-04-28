96f6475222824aa7b5abf7147cc4c74649740ef7
Avvarus <avvaru79.java@mail.com>
2020-04-27 20:46:02 -0400

    Initial commit

---
+++ src/test/java/com/example/zuulapp/ZuulAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zuulapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
